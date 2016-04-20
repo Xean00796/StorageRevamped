812cf8ecf66e947b8dc6abf0394a91b51e75eecf
loza <loza00796@hotmail.co.uk>
2016-04-20 22:23:06 +0100

    Testung

--- src/main/java/com/xeanwolf20/storagerevamped/StorageRevamped.java
+++ src/main/java/com/xeanwolf20/storagerevamped/StorageRevamped.java
@@ -1,13 +1,20 @@
 package com.xeanwolf20.storagerevamped;
 
+import com.xeanwolf20.storagerevamped.reference.Reference;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="storagerevamped", name="Storage Revamped", version="1.9-1.0" )
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class StorageRevamped
 {
+
+    @Mod.Instance(Reference.MOD_ID)
+    public static StorageRevamped instance;
+
+
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
    {
---
+++ src/main/java/com/xeanwolf20/storagerevamped/reference/Reference.java
@@ -0,0 +1,11 @@
+package com.xeanwolf20.storagerevamped.reference;
+
+
+public class Reference
+{
+    public static final String MOD_ID = "storagerevamped";
+    public static final String MOD_NAME = "Storage Revamped";
+    public static final String VERSION = "1.9-1.0";
+    public static final String SERVER_PROXY_CLASS = "com.xeanwolf20.storagerevamped.proxy.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.xeanwolf20.storagerevamped.proxy.ClientProxy";
+}
